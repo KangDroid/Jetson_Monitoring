5b95e59de7a19b44aa8562dbce894a0c03116591
kangdroid <hyunwoo286@gmail.com>
2020-01-24 01:25:48 +0900

    Add CPU Online Information

--- main.cpp
+++ main.cpp
@@ -60,18 +60,70 @@ public:
     }
 };
 
-/*class CPUOnlineInformation {
+class CPUOnlineInformation {
 private:
-};*/
+    int cpu_count;
+    int* cpu_online_fd;
+    bool* cpu_online_val;
+    void registerCPUOnline() {
+        for (int i = 0; i < cpu_count; i++) {
+            string tmp = "/sys/devices/system/cpu/cpu" + to_string(i) +"/online";
+            cpu_online_fd[i] = open(tmp.c_str(), O_RDONLY, 0644);
+            if (cpu_online_fd[i] < 0) {
+                cout << "CPU Online FD Verification Failed" << endl;
+                cout << "Debugging Info: " << endl;
+                cout << "Failed FD --> " << i << endl;
+                cout << "Failed FD Value --> " << cpu_online_fd[i] << endl; 
+            }
+        }
+    }
+
+    bool getEachOnline(int& fd) {
+        char output[20] = {0,};
+        read(fd, output, 19);
+        bool tmp = (output[0] == '1') ? true : false;
+        return tmp;
+    }
+public:
+    CPUOnlineInformation() {
+        this->cpu_count = 4;
+        cpu_online_fd = new int[cpu_count];
+        cpu_online_val = new bool[cpu_count];
+        registerCPUOnline();
+    }
+    ~CPUOnlineInformation() {
+        for (int i = 0; i < cpu_count; i++) {
+            close(cpu_online_fd[i]);
+        }
+        delete[] cpu_online_fd;
+        delete[] cpu_online_val;
+    }
+    bool* getCPUOnlineArray(int& counter) {
+        counter = this->cpu_count;
+        for (int i = 0; i < cpu_count; i++) {
+            cpu_online_val[i] = getEachOnline(cpu_online_fd[i]);
+        }
+        return cpu_online_val;
+    }
+};
 
 int main(void) {
     CPUFrequencyInformation cpf;
+    CPUOnlineInformation cpoi;
     int counter;
     double* tmp = cpf.getCPUFreqArray(counter);
 
     cout << "----------------" << endl;
     for (int i = 0; i < counter; i++) {
         cout << "CPU " << i + 1 << ": " << tmp[i] << "Ghz" << endl;
+    }
+    cout << "----------------" << endl;
+
+    // Online Info
+    bool* tmp_two = cpoi.getCPUOnlineArray(counter);
+    cout << "----------------" << endl;
+    for (int i = 0; i < counter; i++) {
+        cout << "CPU " << i + 1 << ": " << tmp_two[i] << endl;
     }
     cout << "----------------" << endl;
 }
