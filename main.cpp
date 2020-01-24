989d15e23ce91618ca4421503b7f0b1bda0632b8
kangdroid <hyunwoo286@gmail.com>
2020-01-24 21:35:43 +0900

    Actually, this application won't ONLY available for CPU

--- main.cpp
+++ main.cpp
@@ -12,74 +12,75 @@
 
 using namespace std;
 
-class CPUInformation {
+class DeviceInformation {
 private:
     string base_data;
     string final_data;
-    int cpu_count;
-    int* cpu_info_fd;
-    int* cpu_returned_val;
-    void registerCPU() {
-        for (int i = 0; i < cpu_count; i++) {
+    int device_count;
+    int* device_info_fd;
+    int* device_returned_val;
+    void registerDev() {
+        for (int i = 0; i < device_count; i++) {
             // TODO: string changed based on mode;
             string tmp = base_data + to_string(i) + final_data;
 
-            cpu_info_fd[i] = open(tmp.c_str(), O_RDONLY, 0644);
-            if (cpu_info_fd[i] < 0) {
+            device_info_fd[i] = open(tmp.c_str(), O_RDONLY, 0644);
+            if (device_info_fd[i] < 0) {
                 // Log based on mode;
                 cout << "CPU Frequency FD Verification Failed" << endl;
                 cout << "Debugging Info: " << endl;
                 cout << "Failed FD --> " << i << endl;
-                cout << "Failed FD Value --> " << cpu_info_fd[i] << endl; 
+                cout << "Failed FD Value --> " << device_info_fd[i] << endl; 
             }
         }
     }
-    int getEachCPUData(int& fd) {
+    int getEachDevData(int& fd) {
         char output[20] = {0,};
         read(fd, output, 19);
         int tmp = atoi(output);
         return tmp;
     }
 public:
-    CPUInformation(const string& additional_data) {
-        this->base_data = "/sys/devices/system/cpu/cpu";
+    DeviceInformation(string& base_data_dev, const string& additional_data) {
+        this->base_data = base_data_dev;
         this->final_data = additional_data;
-        this->cpu_count = 4;
-        cpu_info_fd = new int[cpu_count];
-        cpu_returned_val = new int[cpu_count];
-        registerCPU();
+        this->device_count = 4;
+        device_info_fd = new int[device_count];
+        device_returned_val = new int[device_count];
+        registerDev();
     }
-    ~CPUInformation() {
-        for (int i = 0; i < cpu_count; i++) {
-            close(cpu_info_fd[i]);
+    ~DeviceInformation() {
+        for (int i = 0; i < device_count; i++) {
+            close(device_info_fd[i]);
         }
-        delete[] cpu_info_fd;
-        delete[] cpu_returned_val;
+        delete[] device_info_fd;
+        delete[] device_returned_val;
     }
 
-    int* getCPUDataArray(int& counter) {
-        counter = this->cpu_count;
-        for (int i = 0; i < cpu_count; i++) {
-            cpu_returned_val[i] = getEachCPUData(cpu_info_fd[i]);
+    int* getDevDataArray(int& counter) {
+        counter = this->device_count;
+        for (int i = 0; i < device_count; i++) {
+            device_returned_val[i] = getEachDevData(device_info_fd[i]);
         }
-        return cpu_returned_val;
+        return device_returned_val;
     }
 };
 
 int main(void) {
-    CPUInformation cpf("/cpufreq/cpuinfo_cur_freq");
-    CPUInformation cpoi("/online");
+    string cpu_device = "/sys/devices/system/cpu/cpu";
+    DeviceInformation cpf(cpu_device, "/cpufreq/cpuinfo_cur_freq");
+    DeviceInformation cpoi(cpu_device, "/online");
     int counter;
 
     // Online Info
-    int* tmp_two = cpoi.getCPUDataArray(counter);
+    int* tmp_two = cpoi.getDevDataArray(counter);
     cout << "----------------" << endl;
     for (int i = 0; i < counter; i++) {
         cout << "CPU " << i + 1 << ": " << ((tmp_two[i]) ? "ON" : "OFF") << endl;
     }
     cout << "----------------" << endl;
 
-    int* tmp = cpf.getCPUDataArray(counter);
+    int* tmp = cpf.getDevDataArray(counter);
 
     cout << "----------------" << endl;
     for (int i = 0; i < counter; i++) {
