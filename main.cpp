cce52f56ef59e6e1d6d7fb79ef74cecbede0e003
kangdroid <hyunwoo286@gmail.com>
2020-01-24 18:40:20 +0900

    Remove duplicate code and global-ify

--- main.cpp
+++ main.cpp
@@ -12,118 +12,77 @@
 
 using namespace std;
 
-class CPUFrequencyInformation {
+class CPUInformation {
 private:
+    string base_data;
+    string final_data;
     int cpu_count;
-    int* cpu_freq_fd;
-    double* cpu_freq_val;
-
-    void registerCPUFreq() {
+    int* cpu_info_fd;
+    int* cpu_returned_val;
+    void registerCPU() {
         for (int i = 0; i < cpu_count; i++) {
-            string tmp = "/sys/devices/system/cpu/cpu" + to_string(i) +"/cpufreq/cpuinfo_cur_freq";
-            cpu_freq_fd[i] = open(tmp.c_str(), O_RDONLY, 0644);
-            if (cpu_freq_fd[i] < 0) {
+            // TODO: string changed based on mode;
+            string tmp = base_data + to_string(i) + final_data;
+
+            cpu_info_fd[i] = open(tmp.c_str(), O_RDONLY, 0644);
+            if (cpu_info_fd[i] < 0) {
+                // Log based on mode;
                 cout << "CPU Frequency FD Verification Failed" << endl;
                 cout << "Debugging Info: " << endl;
                 cout << "Failed FD --> " << i << endl;
-                cout << "Failed FD Value --> " << cpu_freq_fd[i] << endl; 
+                cout << "Failed FD Value --> " << cpu_info_fd[i] << endl; 
             }
         }
     }
-
-    double getEachCPUFreq(int& fd) {
+    int getEachCPUData(int& fd) {
         char output[20] = {0,};
         read(fd, output, 19);
         int tmp = atoi(output);
-        return (tmp / DIVIDER_FACTOR);
-    }
-public:
-    CPUFrequencyInformation() {
-        this->cpu_count = 4;
-        cpu_freq_fd = new int[cpu_count];
-        cpu_freq_val = new double[cpu_count];
-        registerCPUFreq();
-    }
-    ~CPUFrequencyInformation() {
-        for (int i = 0; i < cpu_count; i++) {
-            close(cpu_freq_fd[i]);
-        }
-        delete[] cpu_freq_fd;
-        delete[] cpu_freq_val;
-    }
-    double* getCPUFreqArray(int& counter) {
-        counter = this->cpu_count;
-        for (int i = 0; i < cpu_count; i++) {
-            cpu_freq_val[i] = getEachCPUFreq(cpu_freq_fd[i]);
-        }
-        return cpu_freq_val;
-    }
-};
-
-class CPUOnlineInformation {
-private:
-    int cpu_count;
-    int* cpu_online_fd;
-    bool* cpu_online_val;
-    void registerCPUOnline() {
-        for (int i = 0; i < cpu_count; i++) {
-            string tmp = "/sys/devices/system/cpu/cpu" + to_string(i) +"/online";
-            cpu_online_fd[i] = open(tmp.c_str(), O_RDONLY, 0644);
-            if (cpu_online_fd[i] < 0) {
-                cout << "CPU Online FD Verification Failed" << endl;
-                cout << "Debugging Info: " << endl;
-                cout << "Failed FD --> " << i << endl;
-                cout << "Failed FD Value --> " << cpu_online_fd[i] << endl; 
-            }
-        }
-    }
-
-    bool getEachOnline(int& fd) {
-        char output[20] = {0,};
-        read(fd, output, 19);
-        bool tmp = (output[0] == '1') ? true : false;
         return tmp;
     }
 public:
-    CPUOnlineInformation() {
+    CPUInformation(const string& additional_data) {
+        this->base_data = "/sys/devices/system/cpu/cpu";
+        this->final_data = additional_data;
         this->cpu_count = 4;
-        cpu_online_fd = new int[cpu_count];
-        cpu_online_val = new bool[cpu_count];
-        registerCPUOnline();
+        cpu_info_fd = new int[cpu_count];
+        cpu_returned_val = new int[cpu_count];
+        registerCPU();
     }
-    ~CPUOnlineInformation() {
+    ~CPUInformation() {
         for (int i = 0; i < cpu_count; i++) {
-            close(cpu_online_fd[i]);
+            close(cpu_info_fd[i]);
         }
-        delete[] cpu_online_fd;
-        delete[] cpu_online_val;
+        delete[] cpu_info_fd;
+        delete[] cpu_returned_val;
     }
-    bool* getCPUOnlineArray(int& counter) {
+
+    int* getCPUDataArray(int& counter) {
         counter = this->cpu_count;
         for (int i = 0; i < cpu_count; i++) {
-            cpu_online_val[i] = getEachOnline(cpu_online_fd[i]);
+            cpu_returned_val[i] = getEachCPUData(cpu_info_fd[i]);
         }
-        return cpu_online_val;
+        return cpu_returned_val;
     }
 };
 
 int main(void) {
-    CPUFrequencyInformation cpf;
-    CPUOnlineInformation cpoi;
+    CPUInformation cpf("/cpufreq/cpuinfo_cur_freq");
+    CPUInformation cpoi("/online");
     int counter;
-    double* tmp = cpf.getCPUFreqArray(counter);
+    int* tmp = cpf.getCPUDataArray(counter);
 
     cout << "----------------" << endl;
     for (int i = 0; i < counter; i++) {
-        cout << "CPU " << i + 1 << ": " << tmp[i] << "Ghz" << endl;
+        cout << "CPU " << i + 1 << ": " << tmp[i] / DIVIDER_FACTOR << "Ghz" << endl;
     }
     cout << "----------------" << endl;
 
     // Online Info
-    bool* tmp_two = cpoi.getCPUOnlineArray(counter);
+    int* tmp_two = cpoi.getCPUDataArray(counter);
     cout << "----------------" << endl;
     for (int i = 0; i < counter; i++) {
-        cout << "CPU " << i + 1 << ": " << tmp_two[i] << endl;
+        cout << "CPU " << i + 1 << ": " << ((tmp_two[i]) ? "ON" : "OFF") << endl;
     }
     cout << "----------------" << endl;
 }
